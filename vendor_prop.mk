d81768c6f7902dcf5826534ace52e4cb26d415a1
Soumya Managoli <smanag@codeaurora.org>
2018-12-06 12:51:40 +0530

    whyred: update hal buffering configuration
    Update default hal buffering configurations for sdm660.
    
    Change-Id: I2e95eb5f71343f03d9b266fa66af6d8d48bfeb0f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -76,6 +76,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.adm.buffering.ms=2
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio_hal.in_period_size=144 \
+    vendor.audio_hal.period_multiplier=3 \
+    vendor.audio.adm.buffering.ms=2
+
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.hw.binder.size_kbyte=1024
 
 #Density
