2a372eadb23807f84ea738ff14478cc8ab97c48d
root2raj <ruturaj1999@gmail.com>
2019-07-03 15:53:32 +0530

    Inherit MiuiCamera

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -25,6 +25,9 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 PRODUCT_NAME := lineage_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
