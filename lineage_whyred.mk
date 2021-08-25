fb33cfd0d11f701168702fd3ef94bac9ca03e3bd
root2raj <ruturaj1999@gmail.com>
2021-08-25 07:36:38 +0530

    whyred: Update to coral august fp

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -41,6 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="whyred" \
     PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:11/RQ2A.210505.002/7246365:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys
 
 TARGET_VENDOR := Xiaomi
